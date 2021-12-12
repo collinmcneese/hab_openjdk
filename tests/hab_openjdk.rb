d3c9fec193289281ad50b3cb217f831cd8f52338
Collin McNeese <cmcneese@chef.io>
2021-12-12 08:49:21 -0600

    fixes testing for openjdk
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- tests/hab_openjdk.rb
+++ tests/hab_openjdk.rb
@@ -1,5 +1,13 @@
-describe command('HAB_LICENSE=accept-no-persist && source results/last_build.env && hab pkg exec ${pkg_ident} java -version') do
-  its('exit_status') { should eq 0 }
-  # java -version outputs to stderr
-  its('stderr') { should include 'OpenJDK Runtime Environment' }
+describe.one do
+  describe command('HAB_LICENSE=accept-no-persist && . results/last_build.env && hab pkg exec ${pkg_ident} java -version') do
+    its('exit_status') { should eq 0 }
+    # java -version outputs to stderr
+    its('stderr') { should include 'OpenJDK Runtime Environment' }
+  end
+
+  describe command('HAB_LICENSE=accept-no-persist && source results/last_build.env && hab pkg exec ${pkg_ident} java -version') do
+    its('exit_status') { should eq 0 }
+    # java -version outputs to stderr
+    its('stderr') { should include 'OpenJDK Runtime Environment' }
+  end
 end
